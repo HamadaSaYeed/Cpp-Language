e3e2e4d944756c47280d09dc8dd55134685171a0
Hamada_SaYed <110072418+HamadaSaYeed@users.noreply.github.com>
2022-09-10 03:37:05 +0200

    Add files via upload

---
+++ new/start new.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *p = new int;
+    
+    cin >> *p; // 50
+    
+    cout << *p; // 50
+    
+    return 0;
+}
+
+
+
+
+/*
+
+حجز مساحه في الذاكره بشكل يدوي 
+
+*/
